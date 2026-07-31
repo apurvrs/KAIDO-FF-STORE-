b278d9537507877e993872ba84e2abb8e6c62517
apurvrs <apurvrs16@gmail.com>
2026-07-31 20:50:26 +0530

    Implement animated counter functionality
    Added an animated counter that updates to a target value.

--- js/main.js
+++ js/main.js
@@ -7,3 +7,35 @@ window.addEventListener("load", () => {
     }, 2200);
 
 });
+
+// Animated Counter
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+    const update = () => {
+
+        const target = +counter.getAttribute("data-target");
+
+        const count = +counter.innerText;
+
+        const speed = target / 100;
+
+        if(count < target){
+
+            counter.innerText = Math.ceil(count + speed);
+
+            setTimeout(update,20);
+
+        }else{
+
+            counter.innerText = target.toLocaleString() + "+";
+
+        }
+
+    }
+
+    update();
+
+});
