4f194fc2a8f26da331a893fde9fa07fd6fbf849d
apurvrs <apurvrs16@gmail.com>
2026-08-01 08:54:00 +0530

    Add cart rendering and item removal features
    Implement cart rendering and item removal functionality.

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,57 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+const container = document.getElementById("cart-items");
+const total = document.getElementById("total-price");
+
+function renderCart(){
+
+container.innerHTML="";
+
+let totalPrice=0;
+
+cart.forEach((item,index)=>{
+
+totalPrice += item.price;
+
+container.innerHTML += `
+
+<div class="cart-item">
+
+<img src="${item.image}">
+
+<div class="cart-info">
+
+<h3>${item.name}</h3>
+
+<p>₹${item.price}</p>
+
+</div>
+
+<button class="remove"
+onclick="removeItem(${index})">
+
+Remove
+
+</button>
+
+</div>
+
+`;
+
+});
+
+total.innerText="₹"+totalPrice;
+
+}
+
+function removeItem(index){
+
+cart.splice(index,1);
+
+localStorage.setItem("cart",JSON.stringify(cart));
+
+renderCart();
+
+}
+
+renderCart();
